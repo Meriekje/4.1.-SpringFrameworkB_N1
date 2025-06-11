b832313e7727a9f477df6f9f4d2b8cda8569554f
Meriekje <meritxellara@gmail.com>
2025-06-11 13:38:05 +0200

    Update HelloWorldController.java

--- src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
+++ src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
@@ -3,8 +3,9 @@ package cat.itacademy.s04.t01.n01.controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
